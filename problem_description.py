dbc52c7f349d9d2df0ab972f2e64573534e0bf4f
Kiyou <30902079+Kiyousa@users.noreply.github.com>
2022-07-09 00:35:34 +0800

    Update problem_description.py

--- problem_description.py
+++ problem_description.py
@@ -1,12 +1,12 @@
 # the constants of this problem
 
-
 y = 42
-"y stands for the"
+"y stands for the number of years till the target year"
 
 # T = 85.9e2 * 12.0 / 44.0 * 2.0 * 1 / 3 * 0.8
 T = 85.9e2 * 12.0 / 44.0 * 2.0
-"T stands for the target for carbon sequestration by forest ecosystem"
+"T stands for the target for carbon sequestration by forest ecosystem, unit: million tonnes"
+"The 2.0 stands for the conversion factor which is supposed to be 0.5 showed up in the left of the constrain"
 
 r = 0.04
 "r stands for the interest rate"
